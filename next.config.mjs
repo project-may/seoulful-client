f07578981c947a998031350e140661f3243ca86c
hjpark625 <zero950@gmail.com>
2024-06-18 21:50:59 +0900

    feat: next.config 셋팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'culture.seoul.go.kr',
+        port: '',
+        pathname: '/cmmn/file/getImage.do',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
