ff4325879a2bd8adb5a362e303c93ca773d5d470
김인태 <dlsxody1@naver.com>
2024-07-02 15:05:45 +0900

    style : 마커 크기 고정

--- src/features/map/ui/MarkerPopup.tsx
+++ src/features/map/ui/MarkerPopup.tsx
@@ -13,7 +13,12 @@ export const MarkerPopup = ({
   return (
     <Link href={`/event/${eventId}`}>
       <div className="relative w-[120px] h-[207px] rounded-md bg-white p-[10px] after:content-[''] after:absolute after:right-[50px] after:bottom-[-20px] after:border-[13px] after:border-transparent after:border-t-[10px] after:border-t-[#FFFFFF] shadow-dialog">
-        <Image src={mainImg} alt="행사이미지" width={100} height={140} />
+        {/*
+         */}
+        <div className="relative w-[100px] h-[140px] p-0">
+          <Image src={mainImg} alt="행사이미지" fill />
+        </div>
+
         <div className="text-black-555 font-semibold text-[16px] truncate">
           {eventName}
         </div>
