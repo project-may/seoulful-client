b36b2e94f558daf2b8ca0f94576c65c62d1522e4
김인태 <dlsxody1@naver.com>
2024-07-29 17:50:31 +0900

    feat : naver api 확인

--- src/entities/auth/api/api.ts
+++ src/entities/auth/api/api.ts
@@ -4,8 +4,14 @@ export const loginUser = async ({ provider }: ProviderTypes) => {
   if (provider === 'kakao') {
     const kakaoRestApi = process.env.NEXT_PUBLIC_KAKAO_REST_API;
     const redirectUrl = `${process.env.NEXT_PUBLIC_LOCAL_HOST}/auth/kakao/callback`;
-    const kakaoData = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoRestApi}&redirect_uri=${redirectUrl}&response_type=code&state=kakao`;
-    window.location.href = kakaoData;
+    const kakaoLogin = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoRestApi}&redirect_uri=${redirectUrl}&response_type=code&state=kakao`;
+    window.location.href = kakaoLogin;
+  } else {
+    const naverClientID = process.env.NEXT_PUBLIC_NAVER_LOGIN_CLIENT_ID;
+    const STATE = process.env.NEXT_PUBLIC_NAVER_STATE;
+    const redirectUrl = `${process.env.NEXT_PUBLIC_LOCAL_HOST}/auth/naver/callback`;
+    const naverLogin = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naverClientID}&state=${STATE}&redirect_uri=${redirectUrl}`;
+    window.location.href = naverLogin;
   }
 };
 
