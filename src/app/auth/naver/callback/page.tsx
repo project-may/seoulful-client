22ff8dd39acb981362d2ec951aa784db9354a51f
김인태 <dlsxody1@naver.com>
2024-07-30 15:42:18 +0900

    style : loading component 추가

--- src/app/auth/naver/callback/page.tsx
+++ src/app/auth/naver/callback/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 import { useLoginCheck } from '@/features/auth';
 import { useSocialLogin } from '@/features/auth/model/hooks/useSocialLogin';
+import { LoadingComponent } from '@/shared';
 import React from 'react';
 
 const CallbackPage = () => {
   useSocialLogin();
   useLoginCheck();
-  return <div></div>;
+  return <LoadingComponent />;
 };
 
 export default CallbackPage;
--- src/shared/ui/LoadingComponent.tsx
+++ src/shared/ui/LoadingComponent.tsx
@@ -4,15 +4,15 @@ import Image from 'next/image';
 
 export const LoadingComponent = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div className="fixed flex-col inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <Image
         src={LoadingImage}
-        width={400}
-        height={400}
+        width={200}
+        height={200}
         alt="로딩중입니다."
         className="max-w-full max-h-full"
       />
-      <div className="text-white">데이터를 불러오고 있어요!</div>
+      <div className="text-black mt-4">데이터를 불러오고 있어요!</div>
     </div>
   );
 };
