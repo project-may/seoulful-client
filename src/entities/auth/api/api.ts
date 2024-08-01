3588fdb34dec2a6216d2736d959a4d652ba480d9
김인태 <dlsxody1@naver.com>
2024-08-01 10:09:02 +0900

    modify : workflow env 추가

--- src/entities/auth/api/api.ts
+++ src/entities/auth/api/api.ts
@@ -3,13 +3,13 @@ import { LoginRequest, ProviderTypes, UserDTO } from '@/features/auth';
 export const loginUser = async ({ provider }: ProviderTypes) => {
   if (provider === 'kakao') {
     const kakaoRestApi = process.env.NEXT_PUBLIC_KAKAO_REST_API;
-    const redirectUrl = `${process.env.NEXT_PUBLIC_LOCAL_HOST}/auth/kakao/callback`;
+    const redirectUrl = `${process.env.NEXT_PUBLIC_CLIENT_URL}/auth/kakao/callback`;
     const kakaoLogin = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoRestApi}&redirect_uri=${redirectUrl}&response_type=code&state=kakao`;
     window.location.href = kakaoLogin;
   } else {
     const naverClientID = process.env.NEXT_PUBLIC_NAVER_LOGIN_CLIENT_ID;
     const STATE = process.env.NEXT_PUBLIC_NAVER_STATE;
-    const redirectUrl = `${process.env.NEXT_PUBLIC_LOCAL_HOST}/auth/naver/callback`;
+    const redirectUrl = `${process.env.NEXT_PUBLIC_CLIENT_URL}/auth/naver/callback`;
     const naverLogin = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naverClientID}&state=${STATE}&redirect_uri=${redirectUrl}`;
     window.location.href = naverLogin;
   }
