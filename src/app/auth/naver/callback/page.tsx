8cf5953696bb450cce7da6e4e1a8487080fac19f
김인태 <dlsxody1@naver.com>
2024-08-01 10:20:30 +0900

    modify : useSearchParams 경고로 인한 Suspense 추가

--- src/app/auth/naver/callback/page.tsx
+++ src/app/auth/naver/callback/page.tsx
@@ -2,12 +2,16 @@
 import { useLoginCheck } from '@/features/auth';
 import { useSocialLogin } from '@/features/auth/model/hooks/useSocialLogin';
 import { LoadingComponent } from '@/shared';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 const CallbackPage = () => {
   useSocialLogin();
   useLoginCheck();
-  return <LoadingComponent />;
+  return (
+    <Suspense>
+      <LoadingComponent />
+    </Suspense>
+  );
 };
 
 export default CallbackPage;
