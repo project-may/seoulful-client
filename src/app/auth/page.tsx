b048f514b97f899b2a5de255bc7fc87e1617a84e
김인태 <dlsxody1@naver.com>
2024-08-02 12:41:17 +0900

    style : 이미지 교체 및 css 수정

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -17,23 +17,14 @@ export default function LoginPage() {
         />
         <div className="absolute inset-0 bg-gradient-to-b from-[#30618C]/50 to-[#BF5E70]/70"></div>
       </div>
-      <div className="mt-[165px] ml-[235px] z-10">
-        <Image
-          src="/assets/seoulful-logo.png"
-          alt="seoulful logo"
-          width={91}
-          height={13}
-          className=""
-        ></Image>
-      </div>
-      <div className="ml-[30px] mt-[235px] text-white z-10">
+      <div className="ml-[30px] mt-[160px] text-white z-10">
         <div className="text-inherit text-[25px]">간편하게</div>
         <div className="text-inherit text-[25px]">
           <strong className="text-inherit">소셜로그인</strong>으로
         </div>
         <div className="text-inherit text-[25px]">시작해볼까요?</div>
       </div>
-      <div className="flex mt-[34px] justify-center gap-20 z-10">
+      <div className="flex flex-col items-center mt-[230px] justify-center z-10">
         <LoginButton provider={'naver'} />
         <LoginButton provider={'kakao'} />
       </div>
--- src/features/auth/ui/LoginButton.tsx
+++ src/features/auth/ui/LoginButton.tsx
@@ -6,15 +6,33 @@ import { loginUser } from '@/entities/auth/api/api';
 
 export const LoginButton = ({ provider }: ProviderTypes) => {
   return (
-    <Image
-      src={`/assets/${provider}-logo.png`}
-      alt={`${provider} logo`}
-      width={66}
-      height={66}
-      className="mr-[10px]"
-      onClick={() => {
-        loginUser({ provider });
-      }}
-    ></Image>
+    <>
+      {provider === 'naver' ? (
+        <Image
+          className="mt-20 mb-4"
+          src={`/assets/${provider}-logo.png`}
+          alt={`${provider} login`}
+          width={230}
+          height={76}
+          onClick={() => {
+            loginUser({ provider });
+          }}
+        />
+      ) : (
+        <button className="w-[230px] h-[61px] rounded-[12px] text-[20px] flex items-center bg-[#FEE500]">
+          <Image
+            className="ml-4 mr-8"
+            src={`/assets/kakao-logo.png`}
+            alt={`${provider} login`}
+            width={30}
+            height={76}
+            onClick={() => {
+              loginUser({ provider });
+            }}
+          />
+          카카오 로그인
+        </button>
+      )}
+    </>
   );
 };
