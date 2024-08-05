369bdb0c56f4f61051e0cb9fd436d77ce8dbbcfb
김인태 <dlsxody1@naver.com>
2024-08-05 10:29:31 +0900

    modify : Login button onClick 위치변경

--- src/features/auth/ui/LoginButton.tsx
+++ src/features/auth/ui/LoginButton.tsx
@@ -8,30 +8,34 @@ export const LoginButton = ({ provider }: ProviderTypes) => {
   return (
     <>
       {provider === 'naver' ? (
-        <button className="w-[230px] h-[61px] rounded-[12px] text-[20px] flex items-center bg-[#02C759] text-white mb-4">
+        <button
+          onClick={() => {
+            loginUser({ provider });
+          }}
+          className="w-[230px] h-[61px] rounded-[12px] text-[20px] flex items-center bg-[#02C759] text-white mb-4"
+        >
           <Image
             className="ml-4 mr-6"
             src={`/assets/naver-logo.png`}
             alt={`${provider} login`}
             width={50}
             height={76}
-            onClick={() => {
-              loginUser({ provider });
-            }}
           />
           네이버 로그인
         </button>
       ) : (
-        <button className="w-[230px] h-[61px] rounded-[12px] text-[20px] flex items-center bg-[#FEE500]">
+        <button
+          onClick={() => {
+            loginUser({ provider });
+          }}
+          className="w-[230px] h-[61px] rounded-[12px] text-[20px] flex items-center bg-[#FEE500]"
+        >
           <Image
             className="ml-6 mr-8"
             src={`/assets/kakao-logo.png`}
             alt={`${provider} login`}
             width={30}
             height={76}
-            onClick={() => {
-              loginUser({ provider });
-            }}
           />
           카카오 로그인
         </button>
