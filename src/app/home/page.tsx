491e9d64b4007f83cf489ca13ef9e9da5bf93f54
김인태 <dlsxody1@naver.com>
2024-08-09 14:30:26 +0900

    modify : alert button 삭제

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ import { getHomeEvent } from '@/entities/home';
 import { CategoryList } from '@/features/Category';
 import { GNB } from '@/features/GNB';
 import { LoginStatusBox, ThumbnailList } from '@/features/home';
-import { AlertButton, SearchInput } from '@/shared';
+import { SearchInput } from '@/shared';
 import Link from 'next/link';
 
 const HomePage = async () => {
@@ -13,7 +13,6 @@ const HomePage = async () => {
     <div className="relative flex flex-col gap-y-[20px] p-[30px] overflow-y-scroll">
       <div className="flex justify-between items-start">
         <LoginStatusBox />
-        <AlertButton link="/home/alert" hasAlert />
       </div>
       <div className="flex justify-between items-center gap-x-[10px]">
         <SearchInput icon placeholder="행사명을 입력하세요" />
