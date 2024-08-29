55257b762c8581570dfd305fb9d30497ece4f2e1
dlsxody1 <dlsxody1@naver.com>
2024-08-29 15:34:57 +0900

    modify : 로그인 hook 수정

--- src/features/auth/model/hooks/useLoginCheck.ts
+++ src/features/auth/model/hooks/useLoginCheck.ts
@@ -1,4 +1,5 @@
 'use client';
+import { reissueToken } from '@/entities/auth/api/api';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -6,11 +7,17 @@ export const useLoginCheck = () => {
   const router = useRouter();
   useEffect(() => {
     if (typeof window === 'undefined') return;
-    const checkUser = localStorage.getItem('user');
-    if (checkUser) {
-      router.push('/home');
-    } else {
-      router.push('/auth');
-    }
+    const refreshToken = localStorage.getItem('refreshToken');
+    const validateRefreshToken = async () => {
+      if (refreshToken) {
+        const newToken = await reissueToken(refreshToken);
+        //refresh Token이 있으면서, reissueToken을 했을 때 401이 안나오는 경우에만 push 시켜줌.
+        //401이 나오면 다시 로그인할 수 있게.
+        if (!(typeof newToken === 'number')) {
+          router.back();
+        }
+      }
+    };
+    validateRefreshToken();
   }, []);
 };
