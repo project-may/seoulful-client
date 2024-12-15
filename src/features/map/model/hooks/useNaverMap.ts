d37a095cb1c25e5bbf69c4aadc0b341897b1d94e
hjpark625 <zero950@gmail.com>
2024-12-15 19:29:59 +0900

    chore: console 삭제

--- src/features/map/model/hooks/useNaverMap.ts
+++ src/features/map/model/hooks/useNaverMap.ts
@@ -43,7 +43,6 @@ export const useNaverMap = () => {
       const newLongitude = map.getCenter().x;
       const fetchAddress = async () => {
         const data = await getGeoCode(newLatitude, newLongitude);
-        console.log(data.documents);
         setLocation({
           latitude: newLatitude,
           longitude: newLongitude,
